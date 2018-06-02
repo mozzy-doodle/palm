eb90f3acee91b0a81fbbe8ef612f020a40409910
Michael Alvarez <alvarez.michael.joseph@gmail.com>
2018-06-02 17:18:51 -0500

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ViewChild} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+import {MediaChange, ObservableMedia} from '@angular/flex-layout';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from 'angularfire2/firestore';
+
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +20,78 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  menuSectionCollectionRef: AngularFirestoreCollection<any>;
+  menuItemCollectionRef: AngularFirestoreCollection<any>;
+  menuItems$: Observable<any[]>;
+
+  constructor(public dialog: MatDialog, private afs: AngularFirestore, private observableMedia: ObservableMedia) {
+    this.menuSectionCollectionRef = this.afs.collection<any>('menuSections');
+    this.menuItemCollectionRef = this.afs.collection<any>('menuitems');
+    this.menuItems$ = this.menuItemCollectionRef.valueChanges();
+  }
+  
+
+  addAMenuSectionDialog(): void {
+    let dialogRef = this.dialog.open(AddAMenuSectionDialogComponent, {
+      width: '75%',
+      position: { top: '0' },
+      data: { name: 'name', animal: 'animal' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.addNewMenuItem('Apples');
+      //this.animal = result;
+    });
+  }
+
+  addNewMenuItem(name: string) {
+    if (name && name.trim().length) {
+      //this.menuItemCollectionRef.add({ name: name });
+    }
+  }
+}
+
+/**
+ * Dialog Component
+ */
+@Component({
+  selector: 'app-add-a-category-dialog',
+  templateUrl: './add-a-category-dialog.component.html',
+})
+export class AddAMenuSectionDialogComponent implements OnInit {
+  form: FormGroup;
+  @ViewChild('grid') grid: MatGridList;
+  
+    gridByBreakpoint = {
+      xl: 8,
+      lg: 6,
+      md: 4,
+      sm: 2,
+      xs: 1
+    }
+  
+
+  constructor(
+    public dialogRef: MatDialogRef<AddAMenuSectionDialogComponent>,
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+        name: ['', [Validators.required]]
+    });
+}
+
+save() {
+    this.dialogRef.close(this.form.value);
+}
+
+close() {
+    this.dialogRef.close();
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,76 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {Component} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// tslint:disable-next-line:max-line-length
+import {MatButtonModule, MatMenuModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { AppComponent, AddAMenuSectionDialogComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+
+const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBfPNorx3PuGs095u-g-um_4wT47n2e8jE',
+    authDomain: 'palm-d5c36.firebaseapp.com',
+    databaseURL: 'https://palm-d5c36.firebaseio.com',
+    projectId: 'palm-d5c36',
+    storageBucket: 'palm-d5c36.appspot.com',
+    messagingSenderId: '991626841834'
+  }
+};
+
+const routes: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  // {path: '', component: HeroTopComponent},
+  // {path: AppConfig.routes.heroes, loadChildren: './heroes/heroes.module#HeroesModule'},
+  // {path: AppConfig.routes.error404, component: Error404Component},
+
+  // // otherwise redirect to 404
+  // {path: '**', redirectTo: '/' + AppConfig.routes.error404}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAMenuSectionDialogComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    MatButtonModule,
+     MatMenuModule, 
+     MatInputModule,
+     MatCardModule,
+     MatDialogModule,
+     MatFormFieldModule,
+     MatCheckboxModule,
+     MatGridListModule,
+     BrowserModule, 
+     FormsModule,
+     ReactiveFormsModule,
+     AngularFireModule.initializeApp(environment.firebase),
+     AngularFirestoreModule.enablePersistence(),
+     MatToolbarModule,
+     BrowserAnimationsModule,
+     RouterModule.forRoot(routes),
+      LayoutModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule
   ],
   providers: [],
+  entryComponents: [AddAMenuSectionDialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
